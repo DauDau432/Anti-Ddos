663a7470fbd450eb37ac1612482a85068ea5b1f3
vnuf182 <116556761+vnuf182@users.noreply.github.com>
2022-10-24 17:26:50 +0700

    Update captcha.sh

--- captcha.sh
+++ captcha.sh
@@ -1,20 +1,12 @@
 #!/bin/bash
-
-
 apikey='c773770966510b528aff596071164bbd4b7b3'
 zone='ae0085b1847dc457e9e8c345e798560e'
 email='aulax432@gmail.com'
-
-
 countrylist_willcaptcha='/root/country.txt'
-
-
-################### Captcha cho country:
 ten_file_chua_list=$countrylist_willcaptcha
 echo "`cat $ten_file_chua_list | grep .`" > $ten_file_chua_list
 so_dong_file_chua_list=`cat $ten_file_chua_list | grep . | wc -l`
 dong=1
-
 while [ $dong -le $so_dong_file_chua_list ]
 do
 delaytime=$(( $RANDOM % 9 ))
@@ -27,6 +19,3 @@ countrycaptcha_hientai=$(awk " NR == $dong " $ten_file_chua_list)
 echo "Da bat captcha country: $countrycaptcha_hientai"
 dong=$((dong + 1))
 done
-#echo > $countrylist_willcaptcha
-#echo > $countrylist_captchated
-
