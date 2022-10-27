e9f3205fbdc00b576c6b38295048972cc41ed72a
vnuf182 <116556761+vnuf182@users.noreply.github.com>
2022-10-27 21:44:33 +0700

    Create block-off.sh

---
+++ block-off.sh
@@ -0,0 +1,16 @@
+curl -X PUT "https://api.cloudflare.com/client/v4/zones/ae0085b1847dc457e9e8c345e798560e/firewall/rules/8aa85a9bd5c348069477b578282c7716" \
+     -H "X-Auth-Email: aulax432@gmail.com" \
+     -H "X-Auth-Key: c773770966510b528aff596071164bbd4b7b3" \
+     -H "Content-Type: application/json" \
+     --data '{
+     "action": "block",
+     "priority": 578,
+     "paused":true,
+     "description": "Block All IP",
+     "filter": {
+    "id": "e9400d8671f5410fb00fd894bdaa5e84",
+    "expression": "(not ip.geoip.country in {\"VN\"})",
+    "paused": false,
+    "description": "BLOCK QT"
+  }
+  }'
