2ca7370211d19bad9a425240591e25bb5079301a
vnuf182 <116556761+vnuf182@users.noreply.github.com>
2022-10-27 21:38:03 +0700

    Create get-rules.sh

---
+++ get-rules.sh
@@ -0,0 +1,4 @@
+curl -X GET \
+"https://api.cloudflare.com/client/v4/zones/ae0085b1847dc457e9e8c345e798560e/firewall/rules" \
+-H "X-Auth-Email: aulax432@gmail.com" \
+-H "X-Auth-Key: c773770966510b528aff596071164bbd4b7b3"
