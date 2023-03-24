3a6ec3cc3931fc1149d686d7d25f6aed669fb571
vnuf182 <116556761+vnuf182@users.noreply.github.com>
2023-03-24 16:29:44 +0700

    Update Captcha-off.sh

--- Captcha-off.sh
+++ Captcha-off.sh
@@ -1,17 +1,17 @@
 curl -X PUT \
-"https://api.cloudflare.com/client/v4/zones/<ZONE_ID>/firewall/rules/<RULE_ID>" \
--H "X-Auth-Email: [email đănh nhập Cloudflare]" \
--H "X-Auth-Key: [API Token]"\
+"https://api.cloudflare.com/client/v4/zones/3490413be1c77a344f765f12f4463f20/firewall/rules/930fba41d8b7402d8485248be4dcb676" \
+-H "X-Auth-Email: aulax432@gmail.com" \
+-H "X-Auth-Key: c773770966510b528aff596071164bbd4b7b3"\
 -H "Content-Type: application/json" \
 -d '{
-  "paused": false,
-  "description": "Đặt tên cho Rule",
+  "paused": true,
+  "description": "Captcha Việt Nam",
   "action": "allow",
   "priority": 1,
   "filter": {
-    "id": "<FILTER_ID>",
+    "id": "1f99db421a2a48319e92e1d55905f556",
     "expression": "(http.user_agent contains \"DDoS\"),
     "paused": false,
-    "description": "Đặt tên cho Rule"
+    "description": "Captcha Việt Nam"
   }
 }'
