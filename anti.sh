8710f06998ed2e6a32cc430a7bea4670e090e830
vnuf182 <116556761+vnuf182@users.noreply.github.com>
2023-03-24 15:48:04 +0700

    Update anti.sh

--- anti.sh
+++ anti.sh
@@ -1,6 +1,6 @@
 #!bin/sh
 connect=$(netstat -alntp | grep :443 | wc -l)
-if [ $connect -lt 200 ]
+if [ $connect -lt 100 ]
 then
   cat /root/status.txt | grep "block" && bash <(curl -Ls https://raw.githubusercontent.com/vnuf182/hihi/main/block-off.sh) && echo "allow" > /root/status.txt && clear && echo -e "ANTI DDOS OFF"
 else
