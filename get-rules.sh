d9fd646840469e6ed746b5abcc89b60c7124ac2f
vnuf182 <116556761+vnuf182@users.noreply.github.com>
2023-03-24 14:54:40 +0700

    Update get-rules.sh

--- get-rules.sh
+++ get-rules.sh
@@ -1,4 +1,9 @@
+# curl -X GET \
+# "https://api.cloudflare.com/client/v4/zones/[Zone ID]/firewall/rules" \
+# -H "X-Auth-Email: [email đănh nhập Cloudflare]" \
+# -H "X-Auth-Key: [API Token]"
+
 curl -X GET \
-"https://api.cloudflare.com/client/v4/zones/ae0085b1847dc457e9e8c345e798560e/firewall/rules" \
+"https://api.cloudflare.com/client/v4/zones/3490413be1c77a344f765f12f4463f20/firewall/rules" \
 -H "X-Auth-Email: aulax432@gmail.com" \
 -H "X-Auth-Key: c773770966510b528aff596071164bbd4b7b3"
