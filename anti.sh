c04924beac9cf61773590c443a1129e0aa9b7bb8
vnuf182 <116556761+vnuf182@users.noreply.github.com>
2023-03-24 19:33:20 +0700

    Update anti.sh

--- anti.sh
+++ anti.sh
@@ -1,6 +1,6 @@
 #!bin/sh
 connect=$(netstat -alntp | grep :443 | wc -l)
-if [ $connect -lt 20 ]
+if [ $connect -lt 40 ]
 then
   cat /root/status.txt | grep "block" && bash <(curl -Ls https://raw.githubusercontent.com/vnuf182/hihi/main/block-off.sh) && bash <(curl -Ls https://raw.githubusercontent.com/vnuf182/hihi/main/Captcha-off.sh) && echo "allow" > /root/status.txt && clear && echo -e "ANTI DDOS OFF"
 else
