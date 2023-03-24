d8f339fe638ccbe64c54fe7b19859687c34cf753
vnuf182 <116556761+vnuf182@users.noreply.github.com>
2023-03-24 14:58:05 +0700

    Update and rename anti-captcha.sh to anti.sh

---
+++ anti.sh
@@ -0,0 +1,8 @@
+#!bin/sh
+connect=$(netstat -alntp | grep :443 | wc -l)
+if [ $connect -lt 200 ]
+then
+  cat /root/status-captcha.txt | grep "block" && bash <(curl -Ls https://raw.githubusercontent.com/vnuf182/hihi/main/block-off.sh) && echo "allow" > /root/status.txt && clear && echo -e "ANTI DDOS OFF"
+else
+  cat /root/status-captcha.txt | grep "allow" && bash <(curl -Ls https://raw.githubusercontent.com/vnuf182/hihi/main/block-on.sh) && echo "block" > /root/status.txt && clear && echo -e "ANTI DDOS ON"
+fi
