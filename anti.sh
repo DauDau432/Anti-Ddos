6db01c4327e8ec299a915a18a0967181e658dc58
vnuf182 <116556761+vnuf182@users.noreply.github.com>
2023-03-24 16:31:50 +0700

    Update anti.sh

--- anti.sh
+++ anti.sh
@@ -2,7 +2,7 @@
 connect=$(netstat -alntp | grep :443 | wc -l)
 if [ $connect -lt 100 ]
 then
-  cat /root/status.txt | grep "block" && bash <(curl -Ls https://raw.githubusercontent.com/vnuf182/hihi/main/block-off.sh) && echo "allow" > /root/status.txt && clear && echo -e "ANTI DDOS OFF"
+  cat /root/status.txt | grep "block" && bash <(curl -Ls https://raw.githubusercontent.com/vnuf182/hihi/main/block-off.sh) && bash <(curl -Ls https://raw.githubusercontent.com/vnuf182/hihi/main/Captcha-off.sh) && echo "allow" > /root/status.txt && clear && echo -e "ANTI DDOS OFF"
 else
-  cat /root/status.txt | grep "allow" && bash <(curl -Ls https://raw.githubusercontent.com/vnuf182/hihi/main/block-on.sh) && echo "block" > /root/status.txt && clear && echo -e "ANTI DDOS ON"
+  cat /root/status.txt | grep "allow" && bash <(curl -Ls https://raw.githubusercontent.com/vnuf182/hihi/main/block-on.sh) && bash <(curl -Ls https://raw.githubusercontent.com/vnuf182/hihi/main/Captcha-on.sh) && echo "block" > /root/status.txt && clear && echo -e "ANTI DDOS ON"
 fi
