767e4ae33282b4c1cff5fda7bcaed8dbc7e59933
vnuf182 <116556761+vnuf182@users.noreply.github.com>
2023-03-24 14:56:45 +0700

    Update anti-captcha.sh

--- anti-captcha.sh
+++ anti-captcha.sh
@@ -2,7 +2,7 @@
 connect=$(netstat -alntp | grep :443 | wc -l)
 if [ $connect -lt 200 ]
 then
-  cat /root/status-captcha.txt | grep "block" && bash <(curl -Ls https://raw.githubusercontent.com/vnuf182/hihi/main/block-off.sh) && bash <(curl -Ls https://raw.githubusercontent.com/vnuf182/hihi/main/captcha-off.sh) && echo "allow" > /root/status-captcha.txt && clear && echo -e "ANTI DDOS OFF"
+  cat /root/status-captcha.txt | grep "block" && bash <(curl -Ls https://raw.githubusercontent.com/vnuf182/hihi/main/block-off.sh) && echo "allow" > /root/status-captcha.txt && clear && echo -e "ANTI DDOS OFF"
 else
-  cat /root/status-captcha.txt | grep "allow" && bash <(curl -Ls https://raw.githubusercontent.com/vnuf182/hihi/main/block-on.sh) && bash <(curl -Ls https://raw.githubusercontent.com/vnuf182/hihi/main/captcha-on.sh) && echo "block" > /root/status-captcha.txt && clear && echo -e "ANTI DDOS ON"
+  cat /root/status-captcha.txt | grep "allow" && bash <(curl -Ls https://raw.githubusercontent.com/vnuf182/hihi/main/block-on.sh) && echo "block" > /root/status-captcha.txt && clear && echo -e "ANTI DDOS ON"
 fi
