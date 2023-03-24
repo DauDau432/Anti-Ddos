714dd6865a828ef28768d933067c588be6e79083
vnuf182 <116556761+vnuf182@users.noreply.github.com>
2023-03-24 16:41:02 +0700

    Update Captcha-off.sh

--- Captcha-off.sh
+++ Captcha-off.sh
@@ -1,17 +1,16 @@
-curl -X PUT \
-"https://api.cloudflare.com/client/v4/zones/3490413be1c77a344f765f12f4463f20/firewall/rules/930fba41d8b7402d8485248be4dcb676" \
--H "X-Auth-Email: aulax432@gmail.com" \
--H "X-Auth-Key: c773770966510b528aff596071164bbd4b7b3"\
--H "Content-Type: application/json" \
--d '{
-  "paused": true,
-  "description": "Captcha Việt Nam",
-  "action": "allow",
-  "priority": 1,
-  "filter": {
+curl -X PUT "https://api.cloudflare.com/client/v4/zones/3490413be1c77a344f765f12f4463f20/firewall/rules/930fba41d8b7402d8485248be4dcb676" \
+     -H "X-Auth-Email: aulax432@gmail.com" \
+     -H "X-Auth-Key: c773770966510b528aff596071164bbd4b7b3" \
+     -H "Content-Type: application/json" \
+     --data '{
+     "action": "challenge",
+     "priority": 564,
+     "paused": true,
+     "description": "Captcha Việt Nam",
+     "filter": {
     "id": "1f99db421a2a48319e92e1d55905f556",
-    "expression": "(http.user_agent contains \"DDoS\"),
+    "expression": "(ip.geoip.country eq \"VN\" and http.user_agent contains \"heck\")",
     "paused": false,
     "description": "Captcha Việt Nam"
   }
-}'
+  }'
