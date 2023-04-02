faf411d16ca4c0667ba19499966fff58f9acc916
vnuf182 <116556761+vnuf182@users.noreply.github.com>
2023-04-02 20:56:01 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 yum install net-tools -y && yum install screen -y && yum install jq -y
-wget https://raw.githubusercontent.com/vnuf182/hihi/main/status.txt
-wget https://raw.githubusercontent.com/vnuf182/hihi/main/auto.sh
+status.txt && wget https://raw.githubusercontent.com/vnuf182/hihi/main/status.txt
+rm -rf auto.sh && wget https://raw.githubusercontent.com/vnuf182/hihi/main/auto.sh
 screen bash /root/auto.sh
