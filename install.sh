2de3c2033d952d35b5d9546a66b9972e72361b30
Đậu Đậu <59077968+DauDau432@users.noreply.github.com>
2023-06-23 19:39:50 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,8 @@
-yum install nano -y && yum install net-tools -y && yum install screen -y && yum install jq -y
-rm -rf status.txt && wget https://raw.githubusercontent.com/DauDau432/Anti-Ddos/main/status.txt
-rm -rf auto.sh && wget https://raw.githubusercontent.com/DauDau432/Anti-Ddos/main/auto.sh
+yum install nano -y 
+yum install net-tools -y 
+yum install screen -y 
+yum install jq -y
+rm -rf status.txt 
+wget https://raw.githubusercontent.com/DauDau432/Anti-Ddos/main/status.txt
+rm -rf auto.sh 
+wget https://raw.githubusercontent.com/DauDau432/Anti-Ddos/main/auto.sh
