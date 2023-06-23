06086829375c6df2970c1b2caf198d16ebb91745
Đậu Đậu <59077968+DauDau432@users.noreply.github.com>
2023-06-23 19:33:51 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 yum install net-tools -y && yum install screen -y && yum install jq -y
-status.txt && wget https://raw.githubusercontent.com/DauDau432/API_CFL/main/status.txt
-rm -rf auto.sh && wget https://raw.githubusercontent.com/DauDau432/API_CFL/main/auto.sh
+rm -rf status.txt && wget https://raw.githubusercontent.com/DauDau432/Anti-Ddos/main/status.txt
+rm -rf auto.sh && wget https://raw.githubusercontent.com/DauDau432/Anti-Ddos/main/auto.sh
