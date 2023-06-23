c373a855f68c37c4934e53fad6b3e5f5d219ffe9
Đậu Đậu <59077968+DauDau432@users.noreply.github.com>
2023-06-23 19:31:39 +0700

    Update auto-rule.sh

--- auto-rule.sh
+++ auto-rule.sh
@@ -1,6 +1,6 @@
 #!bin/sh
 while true
 do
-bash <(curl -Ls https://raw.githubusercontent.com/DauDau432/API_CFL/main/anti.sh)
+bash <(curl -Ls https://raw.githubusercontent.com/DauDau432/Anti-Ddos/main/anti.sh)
 sleep 2
 done
