989a6aaaf62b671d665bd2980f8a04219cddf13b
Đậu Đậu <59077968+DauDau432@users.noreply.github.com>
2023-07-08 08:57:58 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ yum install screen -y
 yum install jq -y
 yum install wget -y
 rm -rf status.txt
-wget https://raw.githubusercontent.com/DauDau432/Anti-Ddos/main/status.txt
 rm -rf auto.sh
+wget https://raw.githubusercontent.com/DauDau432/Anti-Ddos/main/status.txt
 wget https://raw.githubusercontent.com/DauDau432/Anti-Ddos/main/auto.sh
 screen -S api-clf -dm bash auto.sh
