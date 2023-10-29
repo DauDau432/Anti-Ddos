a18c4c4948d9d16e8b647b6fa7f79e61cf3271f4
Đậu Đậu <59077968+DauDau432@users.noreply.github.com>
2023-10-29 13:43:33 +0700

    Update install-api.sh

--- install-api.sh
+++ install-api.sh
@@ -7,4 +7,4 @@ rm -rf status.txt
 rm -rf auto.sh
 wget https://raw.githubusercontent.com/DauDau432/Anti-Ddos/main/status.txt
 wget https://raw.githubusercontent.com/DauDau432/Anti-Ddos/main/auto.sh
-screen -S api-clf -dm bash auto.sh
+screen -S API-CLF -dm bash auto.sh && screen -r API-CLF
