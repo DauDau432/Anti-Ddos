b64cb7bb9bae7c86c98fd0c238e038c768896d3a
Đậu Đậu <59077968+DauDau432@users.noreply.github.com>
2024-04-05 11:47:38 +0700

    Update Captcha-on.sh

--- Captcha-on.sh
+++ Captcha-on.sh
@@ -1,4 +1,4 @@
-curl -X PUT "https://api.cloudflare.com/client/v4/zones/adb451c64204498d986435b508aa8a0e/firewall/rules/bf916bc3ff084f32b44d9ffac1fec3da" \
+curl -X PUT "https://api.cloudflare.com/client/v4/zones/dc4829573a8be18a7f2de65921259f9e/firewall/rules/adb451c64204498d986435b508aa8a0e" \
      -H "X-Auth-Email: aulax432@gmail.com" \
      -H "X-Auth-Key: c773770966510b528aff596071164bbd4b7b3" \
      -H "Content-Type: application/json" \
@@ -8,7 +8,7 @@ curl -X PUT "https://api.cloudflare.com/client/v4/zones/adb451c64204498d986435b5
      "paused": false,
      "description": "Managed Challenge Việt Nam",
      "filter": {
-    "id": "1f99db421a2a48319e92e1d55905f556",
+    "id": "bf916bc3ff084f32b44d9ffac1fec3da",
     "expression": "(http.user_agent contains \"Mozilla/5.0 (Macintosh; Intel Mac OS\" and http.user_agent ne \"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:122.0) Gecko/20100101 Firefox/122.0\" and http.user_agent ne \"CheckHost (https://check-host.net/)\" and ip.geoip.country eq \"VN\" and http.request.version in {\"HTTP/1.1\" \"HTTP/2\" \"HTTP/3\"} and http.request.method in {\"GET\" \"POST\"}) or (http.user_agent contains \"Mozilla/5.0 (Linux; Android\" and http.user_agent ne \"CheckHost (https://check-host.net/)\" and http.user_agent ne \"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:122.0) Gecko/20100101 Firefox/122.0\" and ip.geoip.country eq \"VN\" and http.request.version in {\"HTTP/1.1\" \"HTTP/2\" \"HTTP/3\"} and http.request.method in {\"GET\" \"POST\"}) or (http.user_agent contains \"Mozilla/5.0 (iPhone; CPU iPhone OS\" and http.user_agent ne \"CheckHost (https://check-host.net/)\" and http.user_agent ne \"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:122.0) Gecko/20100101 Firefox/122.0\" and ip.geoip.country eq \"VN\" and http.request.version in {\"HTTP/1.1\" \"HTTP/2\" \"HTTP/3\"} and http.request.method in {\"GET\" \"POST\"}) or (http.user_agent contains \"Mozilla/5.0 (Windows NT\" and http.user_agent ne \"CheckHost (https://check-host.net/)\" and http.user_agent ne \"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:122.0) Gecko/20100101 Firefox/122.0\" and ip.geoip.country eq \"VN\" and http.request.version in {\"HTTP/1.1\" \"HTTP/2\" \"HTTP/3\"} and http.request.method in {\"GET\" \"POST\"}) or (http.user_agent ne \"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:122.0) Gecko/20100101 Firefox/122.0\" and http.request.version eq \"HTTP/1.1\" and http.request.method eq \"HEAD\") or (http.user_agent ne \"CheckHost (https://check-host.net/)\" and http.request.version eq \"HTTP/1.1\" and http.request.method eq \"GET\")",
     "paused": false,
     "description": "Managed Challenge Việt Nam"
