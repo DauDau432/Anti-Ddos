6b79f0434359061f8a018a784244e0748946120a
Đậu Đậu <59077968+DauDau432@users.noreply.github.com>
2024-08-10 02:51:55 +0700

    Update block-on.sh

--- block-on.sh
+++ block-on.sh
@@ -5,12 +5,12 @@ curl -X PUT "https://api.cloudflare.com/client/v4/zones/dc4829573a8be18a7f2de659
      --data '{
      "action": "block",
      "priority": 500,
-     "paused": false,
-     "description": "Block quốc tế",
+     "paused": true,
+     "description": "Block Quốc Tế",
      "filter": {
     "id": "b15cd6141671454b924c56eec3b9d713",
     "expression": "(ip.geoip.country ne \"VN\" and http.user_agent contains \"Mozilla/5.0 (Windows NT\" and http.request.version in {\"HTTP/2\" \"HTTP/3\" \"HTTP/1.1\"} and http.request.method in {\"GET\" \"POST\"}) or (ip.geoip.country ne \"VN\" and http.user_agent contains \"Mozilla/5.0 (iPhone; CPU iPhone OS\" and http.request.version in {\"HTTP/2\" \"HTTP/3\" \"HTTP/1.1\"} and http.request.method in {\"GET\" \"POST\"}) or (ip.geoip.country ne \"VN\" and http.user_agent contains \"Mozilla/5.0 (Linux; Android\" and http.request.version in {\"HTTP/2\" \"HTTP/3\" \"HTTP/1.1\"} and http.request.method in {\"GET\" \"POST\"}) or (ip.geoip.country ne \"VN\" and http.user_agent contains \"Mozilla/5.0 (Macintosh; Intel Mac OS\" and http.request.version in {\"HTTP/2\" \"HTTP/3\" \"HTTP/1.1\"} and http.request.method in {\"GET\" \"POST\"})",
     "paused": false,
-    "description": "Block quốc tế"
+    "description": "Block Quốc Tế"
   }
   }'
