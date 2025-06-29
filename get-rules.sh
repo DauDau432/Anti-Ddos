ddf751e366181655131da4f0a5268703e1549584
Đậu Đậu <59077968+DauDau432@users.noreply.github.com>
2025-06-29 20:34:50 +0700

    Update get-rules.sh

--- get-rules.sh
+++ get-rules.sh
@@ -1,4 +1,4 @@
 curl -X GET \
-"https://api.cloudflare.com/client/v4/zones/dc4829573a8be18a7f2de65921259f9e/firewall/rules" \
+"https://api.cloudflare.com/client/v4/zones/db0036a57189911a561e6326e350ea09/firewall/rules" \
 -H "X-Auth-Email: aulax432@gmail.com" \
 -H "X-Auth-Key: c773770966510b528aff596071164bbd4b7b3"
