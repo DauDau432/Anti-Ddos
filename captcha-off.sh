61578894d4f9750b228ecb1da189a6939ef48725
Đậu Đậu <59077968+DauDau432@users.noreply.github.com>
2025-07-04 20:17:00 +0700

    Update captcha-off.sh

--- captcha-off.sh
+++ captcha-off.sh
@@ -1,16 +1,16 @@
-curl -X PUT "https://api.cloudflare.com/client/v4/zones/dc4829573a8be18a7f2de65921259f9e/firewall/rules/adb451c64204498d986435b508aa8a0e" \
-     -H "X-Auth-Email: aulax432@gmail.com" \
-     -H "X-Auth-Key: c773770966510b528aff596071164bbd4b7b3" \
+curl -X PUT "https://api.cloudflare.com/client/v4/zones/db0036a57189911a561e6326e350ea09/firewall/rules/bc8e7716b2bf4c0f9db1ab1318d85c20" \
+     -H "X-Auth-Email: hoanghpqk189@gmail.com" \
+     -H "X-Auth-Key: 4aa920ab578ca860209a05303f794672a605c" \
      -H "Content-Type: application/json" \
      --data '{
      "action": "managed_challenge",
-     "priority": 564,
+     "priority": 3000,
      "paused": true,
-     "description": "Managed Challenge is in Asia",
+     "description": "captcha full trừ /api /admin h2cloud.vn",
      "filter": {
-    "id": "bf916bc3ff084f32b44d9ffac1fec3da",
-    "expression": "(http.user_agent contains \"Mozilla/5.0 (iPhone; CPU iPhone OS\" and ip.geoip.country in {\"CN\" \"DE\" \"HK\" \"ID\" \"JP\" \"KP\" \"KR\" \"SG\" \"TW\" \"US\" \"VN\"} and ip.geoip.continent eq \"AS\" and cf.threat_score le 5 and http.request.version in {\"HTTP/2\" \"HTTP/3\"} and http.request.method in {\"GET\" \"POST\"}) or (http.user_agent contains \"Mozilla/5.0 (Linux; Android\" and ip.geoip.country in {\"CN\" \"DE\" \"HK\" \"ID\" \"JP\" \"KP\" \"KR\" \"TW\" \"US\" \"VN\" \"SG\"} and ip.geoip.continent eq \"AS\" and cf.threat_score le 5 and http.request.version in {\"HTTP/2\" \"HTTP/3\"} and http.request.method in {\"POST\" \"GET\"}) or (http.user_agent contains \"Mozilla/5.0 (Windows NT\" and ip.geoip.country in {\"CN\" \"DE\" \"HK\" \"ID\" \"JP\" \"KP\" \"KR\" \"SG\" \"TW\" \"US\" \"VN\"} and ip.geoip.continent eq \"AS\" and cf.threat_score le 5 and http.request.version in {\"HTTP/2\" \"HTTP/3\"} and http.request.method in {\"GET\" \"POST\"}) or (http.user_agent contains \"Mozilla/5.0 (Macintosh; Intel Mac OS\" and ip.geoip.country in {\"CN\" \"DE\" \"HK\" \"ID\" \"JP\" \"KP\" \"KR\" \"SG\" \"TW\" \"US\" \"VN\"} and ip.geoip.continent eq \"AS\" and cf.threat_score le 5 and http.request.version in {\"HTTP/2\" \"HTTP/3\"} and http.request.method in {\"GET\" \"POST\"})",
+    "id": "fedccd2ad17a496e8600121c12a46af8",
+    "expression": "(http.host eq \"h2cloud.vn\" and not http.request.uri.path contains \"api\") or (http.host eq \"h2cloud.vn\" and not http.request.uri.path contains \"admin\")",
     "paused": false,
-    "description": "Managed Challenge is in Asia"
+    "description": "captcha full trừ /api /admin h2cloud.vn"
   }
   }'
