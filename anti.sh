f9f43a6e714d72ec9ddef312059720dbf3b9187c
Đậu Đậu <59077968+DauDau432@users.noreply.github.com>
2025-07-04 21:13:19 +0700

    Update anti.sh

--- anti.sh
+++ anti.sh
@@ -1,7 +1,8 @@
-#!bin/sh
-connect=$(netstat -alntp | grep :443 | wc -l)
-if [ $connect -lt 100 ]
-then
+#!/bin/sh
+connect_443=$(netstat -alntp | grep :443 | wc -l)
+connect_est=$(netstat -tun | grep ESTABLISHED | wc -l)
+
+if [ $connect_443 -lt 100 ] && [ $connect_est -lt 100 ]; then
   cat /root/status.txt | grep "block" && bash <(curl -Ls https://raw.githubusercontent.com/DauDau432/Anti-Ddos/main/captcha-off.sh) && echo "allow" > /root/status.txt && clear && echo -e "ANTI DDOS OFF"
 else
   cat /root/status.txt | grep "allow" && bash <(curl -Ls https://raw.githubusercontent.com/DauDau432/Anti-Ddos/main/captcha-on.sh) && echo "block" > /root/status.txt && clear && echo -e "ANTI DDOS ON"
