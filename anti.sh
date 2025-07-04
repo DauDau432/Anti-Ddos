110409ef338be1f54933f786cbe60333a166465c
Đậu Đậu <59077968+DauDau432@users.noreply.github.com>
2025-07-04 20:22:10 +0700

    Update anti.sh

--- anti.sh
+++ anti.sh
@@ -1,8 +1,8 @@
 #!bin/sh
 connect=$(netstat -alntp | grep :443 | wc -l)
-if [ $connect -lt 2000 ]
+if [ $connect -lt 200 ]
 then
-  cat /root/status.txt | grep "block" && bash <(curl -Ls https://raw.githubusercontent.com/DauDau432/Anti-Ddos/main/block-off.sh) && bash <(curl -Ls https://raw.githubusercontent.com/DauDau432/Anti-Ddos/main/captcha-off.sh) && bash <(curl -Ls https://raw.githubusercontent.com/DauDau432/Anti-Ddos/main/challenge-off.sh) && echo "allow" > /root/status.txt && clear && echo -e "ANTI DDOS OFF"
+  cat /root/status.txt | grep "block" && bash <(curl -Ls https://raw.githubusercontent.com/DauDau432/Anti-Ddos/main/captcha-off.sh) && echo "allow" > /root/status.txt && clear && echo -e "ANTI DDOS OFF"
 else
-  cat /root/status.txt | grep "allow" && bash <(curl -Ls https://raw.githubusercontent.com/DauDau432/Anti-Ddos/main/block-on.sh) && bash <(curl -Ls https://raw.githubusercontent.com/DauDau432/Anti-Ddos/main/captcha-on.sh) && bash <(curl -Ls https://raw.githubusercontent.com/DauDau432/Anti-Ddos/main/challenge-on.sh) && echo "block" > /root/status.txt && clear && echo -e "ANTI DDOS ON"
+  cat /root/status.txt | grep "allow" && && bash <(curl -Ls https://raw.githubusercontent.com/DauDau432/Anti-Ddos/main/captcha-on.sh) && echo "block" > /root/status.txt && clear && echo -e "ANTI DDOS ON"
 fi
